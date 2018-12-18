856ed5c8501e241b938e11fbcb3b3ac2a6c3233f
padmanabhan21 <padmanabhan210193@gmail.com>
2018-12-18 11:09:18 +0530

    new cheanges

--- src/pages/doctorsdetails/doctorsdetails.ts
+++ src/pages/doctorsdetails/doctorsdetails.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 import { IonicPage, NavController, NavParams, ActionSheetController, ModalController } from 'ionic-angular';
 import { FeedbackPage } from '../feedback/feedback';
+// import { StarRatingModule } from 'ionic3-star-rating';
+// import { Events } from 'ionic-angular';
+
 /**
  * Generated class for the DoctorsdetailsPage page.
  *
@@ -12,24 +17,46 @@ import { FeedbackPage } from '../feedback/feedback';
 @Component({
   selector: 'page-doctorsdetails',
   templateUrl: 'doctorsdetails.html',
+  
 })
 export class DoctorsdetailsPage {
   public totalclinic = 2;
-
+    public clenicimages:any []
+    public slides:any[]
   constructor(public navCtrl: NavController, public navParams: NavParams,
   public actionsheetCtrl: ActionSheetController,
   public modalCtrl: ModalController) {
+    // events.subscribe('star-rating:changed', (starRating) => {console.log(starRating)});
+    
   }
+  @ViewChild('mySlider') slider: Slides;
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DoctorsdetailsPage');
+    this.clenicimages=[
+      {img:"../assets/img/app1.jpg"},
+      {img:"../assets/img/app1.jpg"},
+      {img:"../assets/img/app1.jpg"},
+      {img:"../assets/img/app4.jpg"},
+      {img:"../assets/img/app5.jpg"}
+    ];
+  }
+  getRandomIndex(): number {
+    // uses your list.length to get a random value within the range
+    return Math.floor(Math.random() * this.slides.length)
   }
   navtofeedback(){
     if(this.totalclinic >= 2){
     this.feedbackforclinic();
     }
   }
-
+  newSlide() {
+    this.slider.slideTo(this.getRandomIndex(),500)
+  }
+  closeModal(){
+    this.navCtrl.pop();
+  }
+   
   feedbackforclinic(){
     let actionSheet = this.actionsheetCtrl.create({
       title: 'Which clinic did you visit?',
