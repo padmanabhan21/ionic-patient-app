2e5c3624d2b093e8f14c015b6c3563370fe26ad0
padmanabhan21 <padmanabhan210193@gmail.com>
2018-12-20 18:32:07 +0530

    new cheanges

--- src/pages/searchdoctor/searchdoctor.ts
+++ src/pages/searchdoctor/searchdoctor.ts
@@ -15,34 +15,37 @@ import { ListofdoctorsPage } from '../listofdoctors/listofdoctors';
 })
 export class SearchdoctorPage {
 
-  public specialist:any=[
-    {name:"Women's Health",icon:"../assets/img/women's-health.png"},
-    {name:"Skin & Hair",icon:"../assets/img/skin-hair.png"},
-    {name:"Child Specialist",icon:"../assets/img/women's-health.png"},
-    {name:"General Doctor",icon:"../assets/img/women's-health.png"},
-    {name:"Dental Care",icon:"../assets/img/women's-health.png"},
-    {name:"Ear Nose Throat",icon:"../assets/img/women's-health.png"},
-    {name:"Homoeopathy",icon:"../assets/img/women's-health.png"},
-    {name:"Bone and Joints",icon:"../assets/img/women's-health.png"},
-    {name:"Sex Specialist",icon:"../assets/img/women's-health.png"},
-    {name:"Eye specialist",icon:"../assets/img/women's-health.png"},
-    {name:"Digestive Issues",icon:"../assets/img/women's-health.png"},
+  public specialist: any = [
+    { name: "Women's Health", icon: "../assets/img/women's-health.png" },
+    { name: "Skin & Hair", icon: "../assets/img/skin-hair.png" },
+    { name: "Child Specialist", icon: "../assets/img/women's-health.png" },
+    { name: "General Doctor", icon: "../assets/img/women's-health.png" },
+    { name: "Dental Care", icon: "../assets/img/women's-health.png" },
+    { name: "Ear Nose Throat", icon: "../assets/img/women's-health.png" },
+    { name: "Homoeopathy", icon: "../assets/img/women's-health.png" },
+    { name: "Bone and Joints", icon: "../assets/img/women's-health.png" },
+    { name: "Sex Specialist", icon: "../assets/img/women's-health.png" },
+    { name: "Eye specialist", icon: "../assets/img/women's-health.png" },
+    { name: "Digestive Issues", icon: "../assets/img/women's-health.png" },
   ];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalctrl:ModalController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalctrl: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchdoctorPage');
   }
-  navspecialist(){
+  navspecialist() {
     alert("woking")
     console.log("clicked")
-    let navtodoc = this.modalctrl.create(ListofdoctorsPage,undefined,{cssClass:"modal-fullscreen"});
+    let navtodoc = this.modalctrl.create(ListofdoctorsPage, undefined, { cssClass: "modal-fullscreen" });
     navtodoc.present();
     // this.navCtrl.push("DoctorspacialistPage");
   }
-  closeModal(){
+  closeModal() {
     this.navCtrl.pop();
   }
+  //
+  //specialist array
+
 
 }
