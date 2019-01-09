7a6e59dd18f026408c300dd4e5b9f06f03687df6
venkateshn692 <venkateshn692@gmail.com>
2019-01-09 19:39:56 +0530

    newcheanges

--- src/pages/medicalslide/medicalslide.ts
+++ src/pages/medicalslide/medicalslide.ts
@@ -17,6 +17,10 @@ import { Slides } from 'ionic-angular';
 })
 export class MedicalslidePage {
   @ViewChild('slides') slides: Slides;
+
+  public currentIdex=0;
+  public totalQuestion=6;
+  public medicalslide:any={}
   public medical_profile:any =
   {
     "allergies":"",
@@ -37,20 +41,56 @@ export class MedicalslidePage {
   closeModal(){
     this.navCtrl.pop();
   }
+  illness(param){
+    this.medicalslide.illness = param;
+  }
+  injuries(param){
+    this.medicalslide.injuries = param;
+  }
+  allergic(param){
+    this.medicalslide.allergic = param;
+  }
+  surgeries(param){
+    this.medicalslide.surgeries = param;
+  }
+  medicines(param){
+    this.medicalslide.medicines = param;
+  }
+  medications(param){
+    this.medicalslide.medications = param;
+  }
 
   next() {
     this.slides.slideNext();
+    if(this.currentIndex < 5){
+      this.totalQuestion = this.totalQuestion - 1;
+    }
   }
-
   prev() {
     this.slides.slidePrev();
+    if(this.currentIndex > 0){
+      this.totalQuestion = this.totalQuestion + 1;
+    }
   }
 
   public currentIndex:any;
-  slideChanged() {
+  public showsave:boolean=false;
+  medslideChanged() {
     let currentIndex = this.slides.getActiveIndex();
     console.log('Current index is', currentIndex);
     this.currentIndex = currentIndex;
+    if(this.currentIndex == 5){
+      this.showsave = true;
+    }else{
+      this.showsave = false;
+    }
+    console.log('Total Question is',this.totalQuestion);
+  } 
+
+  save(){
+    
+    alert('hellodarling')
+    console.log("medical status",JSON.stringify(this.medicalslide));
   }
 
   yes(){
